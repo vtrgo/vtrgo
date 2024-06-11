96e64ebc2f0b96e9eead404c53a1ccf5c58dcb1e
Justin <justinfrombeamsville@gmail.com>
2024-06-11 09:07:46 -0400

    cleanup

---
+++ db/configdb.go
@@ -0,0 +1,60 @@
+package plcdb
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// ConfigVariable represents a configuration variable stored in the database
+type ConfigVariable struct {
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// InitConfigDB initializes the configuration database
+func InitConfigDB(db *sql.DB) error {
+	query := `
+	CREATE TABLE IF NOT EXISTS config_variables (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL UNIQUE,
+		value TEXT NOT NULL
+	);
+	`
+	_, err := db.Exec(query)
+	return err
+}
+
+// InsertConfigVariable inserts a new configuration variable into the database
+func InsertConfigVariable(db *sql.DB, name, value string) error {
+	query := `INSERT INTO config_variables (name, value) VALUES (?, ?)`
+	_, err := db.Exec(query, name, value)
+	return err
+}
+
+// RemoveConfigVariable removes a configuration variable from the database by name
+func RemoveConfigVariable(db *sql.DB, name string) error {
+	query := `DELETE FROM config_variables WHERE name = ?`
+	_, err := db.Exec(query, name)
+	return err
+}
+
+// FetchConfigVariables retrieves all configuration variables from the database
+func FetchConfigVariables(db *sql.DB) ([]ConfigVariable, error) {
+	rows, err := db.Query(`SELECT id, name, value FROM config_variables`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var variables []ConfigVariable
+	for rows.Next() {
+		var variable ConfigVariable
+		if err := rows.Scan(&variable.ID, &variable.Name, &variable.Value); err != nil {
+			return nil, err
+		}
+		variables = append(variables, variable)
+	}
+	return variables, nil
+}
---
+++ plc/plc.go
@@ -0,0 +1,74 @@
+// plc/plc.go
+package plc
+
+import (
+	"github.com/danomagnum/gologix"
+)
+
+type PLC struct {
+	client *gologix.Client
+}
+
+// NewPLC creates a new PLC instance with the specified IP address.
+func NewPLC(ip string) *PLC {
+	client := gologix.NewClient(ip)
+	return &PLC{client: client}
+}
+
+// Connect establishes a connection to the PLC.
+func (plc *PLC) Connect() error {
+	return plc.client.Connect()
+}
+
+// Disconnect closes the connection to the PLC.
+func (plc *PLC) Disconnect() {
+	plc.client.Disconnect()
+}
+
+// ReadTagInt32 reads an int32 value from the specified tag.
+func (plc *PLC) ReadTagInt32(tagName string) (int32, error) {
+	var tagValue int32
+	err := plc.client.Read(tagName, &tagValue)
+	return tagValue, err
+}
+
+// WriteTagInt32 writes an int32 value to the specified tag.
+func (plc *PLC) WriteTagInt32(tagName string, value int32) error {
+	return plc.client.Write(tagName, value)
+}
+
+// ReadTagFloat32 reads a float32 value from the specified tag.
+func (plc *PLC) ReadTagFloat32(tagName string) (float32, error) {
+	var tagValue float32
+	err := plc.client.Read(tagName, &tagValue)
+	return tagValue, err
+}
+
+// WriteTagFloat32 writes a float32 value to the specified tag.
+func (plc *PLC) WriteTagFloat32(tagName string, value float32) error {
+	return plc.client.Write(tagName, value)
+}
+
+// ReadTagBool reads a boolean value from the specified tag.
+func (plc *PLC) ReadTagBool(tagName string) (bool, error) {
+	var tagValue bool
+	err := plc.client.Read(tagName, &tagValue)
+	return tagValue, err
+}
+
+// WriteTagBool writes a boolean value to the specified tag.
+func (plc *PLC) WriteTagBool(tagName string, value bool) error {
+	return plc.client.Write(tagName, value)
+}
+
+// ReadTagBool reads a boolean value from the specified tag.
+func (plc *PLC) ReadTagString(tagName string) (string, error) {
+	var tagValue string
+	err := plc.client.Read(tagName, &tagValue)
+	return tagValue, err
+}
+
+// WriteTagBool writes a boolean value to the specified tag.
+func (plc *PLC) WriteTagString(tagName string, value string) error {
+	return plc.client.Write(tagName, value)
+}
